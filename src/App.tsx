527ceba95ad86591a262c2191bf6f0275f849a4d
AlexRado <kia8247@gmail.com>
2023-05-06 14:17:38 +0300

    ...

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import { useState } from 'react';
 import './App.css';
-import Todos from './todo/ Components/Todos';
 
 const App = () => {
   return (
     <div>
-      {/* <h1>Change</h1> */}
-      <Todos />
+      <h1>Change</h1>
     </div>
   );
 };
