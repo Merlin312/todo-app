9e9569f02524cbf21c5a65a45af99aa472c5b37f
AlexRado <kia8247@gmail.com>
2023-05-09 14:20:18 +0300

    write code practice get

--- src/js-practice/UserList.tsx
+++ src/js-practice/UserList.tsx
@@ -1,40 +1,38 @@
-import { useEffect, useState } from 'react';
 import axios from 'axios';
+import { useEffect, useState } from 'react';
 
-function UserList() {
+const UserList = () => {
   const [users, setUsers] = useState([]);
 
   useEffect(() => {
     axios
       .get('https://jsonplaceholder.typicode.com/users')
-      .then((response) => setUsers(response.data))
-      .catch((error) => console.log(error));
+      .then((response) => {
+        setUsers(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }, []);
-
   return (
     <div>
       <thead>
         <tr>
           <th>#</th>
           <th>Name</th>
-          <th>Username</th>
           <th>Email</th>
-          <th>Phone</th>
         </tr>
       </thead>
       <tbody>
-        {users.map((user, index) => (
+        {users.map((user) => (
           <tr key={user.id}>
             <td>{user.id}</td>
             <td>{user.name}</td>
-            <td>{user.username}</td>
             <td>{user.email}</td>
-            <td>{user.phone}</td>
           </tr>
         ))}
       </tbody>
     </div>
   );
-}
-
+};
 export default UserList;
