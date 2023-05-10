279aae87d1267c73dc9877301ef4fe23b5b39647
AlexRado <kia8247@gmail.com>
2023-05-10 12:26:53 +0300

    add component SayHelloName

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import './App.css';
-import PracticeTodo from './js-practice/PracticeTodo';
+import SayHelloName from './js-practice/SayHelloName';
 
 const App = () => {
+  const name = 'Ivan';
   return (
     <div>
       {/* <h1>Something</h1> */}
-      <PracticeTodo />
+      <SayHelloName name={name} />
     </div>
   );
 };
---
+++ src/js-practice/SayHelloName.tsx
@@ -0,0 +1,8 @@
+const SayHelloName = (props) => {
+  return (
+    <div>
+      <h1>{`Helle, ${props.name}`}</h1>
+    </div>
+  );
+};
+export default SayHelloName;
