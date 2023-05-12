e25bfd7e486765cf13051c2257e2eb07ab3b0dcc
AlexRado <kia8247@gmail.com>
2023-05-12 14:55:18 +0300

    practice try catch error throw

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import './App.css';
-import FormPractice from './js-practice/FormPractice';
+import PracticeTry from './practice/js-practice/PracticeTry';
 
 const App = () => {
   setInterval(() => {
     console.clear();
-  }, 30000);
+  }, 60000);
   return (
     <div>
-      {/* <h1>Додаток для лічильника кліків</h1> */}
-      <FormPractice />
+      {/* <h1>Something</h1> */}
+      <PracticeTry />
     </div>
   );
 };
---
+++ src/practice/js-practice/PracticeTry.tsx
@@ -0,0 +1,30 @@
+const PracticeTry = () => {
+  try {
+    const result = 5 / 'eeee';
+    throw new Error('Помилка!');
+    console.log(result);
+  } catch (error) {
+    console.log(error.message);
+    // console.error(error);
+    // console.log('Помилка!!!!');
+  }
+
+  // let obj = { name: 'Alex', age: 34, email: 'gpt@gmail.com' };
+  // for (const {} in obj) {
+  //   if (obj.hasOwnProperty(key)) {
+  //     console.log(key, obj[key]);
+  //   }
+  // }
+
+  // Object.keys(obj).forEach((key) => {
+  //   console.log(key, obj[key]);
+  // });
+
+  return (
+    <div>
+      <h1>Hee</h1>
+    </div>
+  );
+};
+
+export default PracticeTry;
