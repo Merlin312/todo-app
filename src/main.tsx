77b46effc1b5a3e0b60bcc949b92cdbf7b2ca218
AlexRado <kia8247@gmail.com>
2023-06-16 21:38:15 +0300

    fixed error

--- src/main.tsx
+++ src/main.tsx
@@ -1,29 +1,49 @@
-// import React from 'react'
-// import ReactDOM from 'react-dom/client'
-// import App from './App'
-// import './index.css'
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import App from './App';
+// import './index.css';
 
 // ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 //   <React.StrictMode>
 //     <App />
-//   </React.StrictMode>,
-// )
+//   </React.StrictMode>
+// );
+// import React from 'react';
+// import { render } from 'react-dom';
+// import { createStore } from 'redux';
+// import { Provider } from 'react-redux';
+// import rootReducer from './practice/todo-app/reducers/index';
+// import App from './App';
+// import './index.css';
+
+// const store = createStore(
+//   rootReduce,
+//   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+// );
+
+// render(
+//   <Provider store={store}>
+//     <App />
+//   </Provider>,
+//   document.getElementById('root')
+// );
 import React from 'react';
-import { render } from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import rootReducer from './practice/todo-app/reducers/index';
 import App from './App';
 import './index.css';
+import rootReducer from './practice/todo-app/reducers';
 
 const store = createStore(
-  rootReduce,
+  rootReducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
-render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>
 );
--- src/practice/todo-app/components/Todo.tsx
+++ src/practice/todo-app/components/Todo.tsx
@@ -1,19 +1,30 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import { toggleTodo } from '../actions';
+import './Todo.css';
 
 const Todo = ({ todo }) => {
   const dispatch = useDispatch();
 
+  const handleClick = () => {
+    dispatch(toggleTodo(todo.id));
+  };
+
   return (
-    <li
-      onClick={() => dispatch(toggleTodo(todo.id))}
-      style={{
-        textDecoration: todo.completed ? 'line-through' : 'none',
-      }}
-    >
-      {todo.content}
-    </li>
+    <div className="todo-container">
+      <li onClick={handleClick} className={todo.completed ? 'completed' : ''}>
+        {todo.content}
+      </li>
+      <button
+        className="delete-button"
+        onClick={(e) => {
+          e.stopPropagation();
+          // Handle delete button click
+        }}
+      >
+        X
+      </button>
+    </div>
   );
 };
 
