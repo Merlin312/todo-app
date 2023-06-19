fa9a120e131e93cda74b9c95e9ef05cd6c331511
AlexRado <kia8247@gmail.com>
2023-06-19 14:59:20 +0300

    add button <AddRandomTodo>

--- src/practice/todo-app/components/Buttons.tsx
+++ src/practice/todo-app/components/Buttons.tsx
@@ -4,6 +4,7 @@ import SortTodo from './buttons/SortTodo';
 import ThemeSlider from './buttons/ThemeSlider';
 import UpperCase from './buttons/UpperCase';
 import RemoveAll from './buttons/RemoveAll';
+import AddRandomTodo from './buttons/AddRandomTodo';
 
 const Buttons = () => {
   const theme = useSelector((state) => state.themes);
@@ -15,6 +16,7 @@ const Buttons = () => {
       <UpperCase theme={theme} />
       <LowerCase theme={theme} />
       <RemoveAll theme={theme} />
+      <AddRandomTodo />
     </div>
   );
 };
---
+++ src/practice/todo-app/components/buttons/AddRandomTodo.tsx
@@ -0,0 +1,7 @@
+const AddRandomTodo = () => {
+  const handleAddRandomTodo = () => {
+    console.log('something');
+  };
+  return <button onClick={handleAddRandomTodo}>RandomTodo</button>;
+};
+export default AddRandomTodo;
